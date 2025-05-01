4c70df45ed30224670e90573c87bb00004784b9b
rakesh4stepsdigital <rakesh@4stepsdigital.com>
2025-05-01 08:23:42 +0530

    add pagination in blog page

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -84,13 +84,13 @@ const BlogPage = () => {
           </div>
 
           {/* Pagination */}
-          <div className="flex justify-center items-center space-x-2 mt-10 mb-6">
+           <div className="flex justify-center items-center space-x-2 mt-10 mb-6">
             <button
               onClick={() => handlePageChange(currentPage - 1)}
               disabled={currentPage === 1}
-              className="px-4 py-2 text-sm bg-gray-200 rounded hover:bg-gray-300 disabled:opacity-50"
+              className="px-4 py-1.5 text-xs font-medium bg-gray-200 rounded hover:bg-gray-300 disabled:opacity-50"
             >
-              Prev
+              Prev...
             </button>
 
             {[...Array(totalPages)].map((_, index) => {
@@ -99,10 +99,10 @@ const BlogPage = () => {
                 <button
                   key={index}
                   onClick={() => handlePageChange(pageNum)}
-                  className={`px-3 py-1 text-sm rounded border ${
+                  className={`px-3 py-1 text-xs rounded border font-medium ${
                     currentPage === pageNum
                       ? "bg-blue-500 text-white"
-                      : "bg-white text-black hover:bg-gray-100"
+                      : "bg-white text-gray-700 hover:bg-gray-100"
                   }`}
                 >
                   {pageNum}
@@ -113,9 +113,9 @@ const BlogPage = () => {
             <button
               onClick={() => handlePageChange(currentPage + 1)}
               disabled={currentPage === totalPages}
-              className="px-4 py-2 text-sm bg-gray-200 rounded hover:bg-gray-300 disabled:opacity-50"
+              className="px-4 py-1.5 text-xs font-medium bg-gray-200 rounded hover:bg-gray-300 disabled:opacity-50"
             >
-              Next
+              Next...
             </button>
           </div>
         </div>
